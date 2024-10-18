52a53096036d9b8a3753d40c13c2f2747dbf58f6
branaji21 <20231926@nbsc.edu.ph>
2024-10-18 22:57:13 +0800

    creating a class on js to use it to fetch data for applets

---
+++ Dynamic card/applet.js
@@ -0,0 +1,8 @@
+class AppletCard {
+    constructor(title,icon, description, link) {
+        this.title = title;
+        this.icon = icon;
+        this.description = description;
+        this.link = link;
+    }
+}
