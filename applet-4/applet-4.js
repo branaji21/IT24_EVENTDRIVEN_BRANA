2919c4dc096eafb77eb64ae9cd33389f9257cf02
branaji21 <20231926@nbsc.edu.ph>
2024-10-18 18:47:41 +0800

    added a asynchronous method to display the list student and retrieve student data

--- applet-4/applet-4.js
+++ applet-4/applet-4.js
@@ -4,6 +4,11 @@ class StudentList {
         this.students = [];
         this.init();
     }
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
 
    
 }
